e9abc86eb9e0ab482f6789b8180324a2cbdecf82
Temirlan24 <52044447+Temirlan24@users.noreply.github.com>
2019-07-02 17:00:32 +0600

    Practica2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -23,13 +23,20 @@ namespace ConsoleApp1
              length = 2 * Math.PI * radius;
              Console.WriteLine(length);
              Console.ReadKey();*/
-            double length;
+            /*3. double length;
             double metr;
             length = double.Parse(Console.ReadLine());
             metr = length / 100;
-            Console.WriteLine(metr);
+            Console.Write(metr);
+            Console.WriteLine(" метр");
             Console.ReadKey();
-
+            */
+            /* 4. int day = 234;
+            double week = day / 7;
+            Console.Write("Прошло ");
+            Console.Write(week);
+            Console.WriteLine(" недель");
+            Console.ReadKey();*/
             /*5. int number;
             number = int.Parse(Console.ReadLine());
             int digit1, digit2,sum;
@@ -42,7 +49,43 @@ namespace ConsoleApp1
             Console.WriteLine(sum);
             Console.ReadKey();*/
 
+            /*6. int number = int.Parse(Console.ReadLine());
+            int digit1, digit2, digit3, digit4;
+            digit1 = number / 1000;
+            digit2 = (number / 100) % 10;
+            digit3 = (number / 10) % 10;
+            digit4 = number % 10;
+            int sum , product;
+            sum = digit1 + digit2 + digit3 + digit4;
+            product = digit1 * digit2 * digit3 * digit4;
+            Console.WriteLine("Сумма = {0}", sum);
+            Console.WriteLine("Произведение = {0}", product);
+            Console.ReadKey();*/
+
+            /* 7. int number = 456;
+            int digit1, digit2, digit3;
+            digit1 = number / 100;
+            digit2=(number/10)% 10;
+            digit3 = number % 10;
+            Console.Write("X = {0}{1}{2}", digit1, digit3, digit2);
+            Console.ReadKey();*/
+            //8
+            bool X = true, Y = false;
+            if (!X && !Y)
+            {
+                Console.WriteLine("A variant");
+            }
+            if(X ||(!X && Y))
+            {
+                Console.WriteLine("B variant");
+            }
+            if ((!X && Y) || Y)
+            {
+                Console.WriteLine("C variant");
+            }
+            Console.ReadKey();
         }
 
     }
 }
+
