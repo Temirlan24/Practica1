fd7a0c4a60ef052e86fd831dc5e64bd1b6a3bd00
Temirlan24 <52044447+Temirlan24@users.noreply.github.com>
2019-07-07 23:01:53 +0600

    Сделал практику №3, задания №1-5.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
@@ -10,82 +11,189 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            /* 1. Console.WriteLine("Введите данные в массив из 5 элементов:");
+            int[] A = new int[5];
+            int sumofAllElementA = 0, maxA, minA, sumEvenNumbers = 0;
+            double sumofAllElementB = 0, maxB, minB, sumOddColums = 0;
+            double[,] B = new double[3, 4];
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Clear();
+            for (int i = 0; i < A.Length; i++)
+            {
+                Console.Write(A[i] + " ");
+                if (A[i] % 2 == 0)
+                {
+                    sumEvenNumbers += A[i];
+                }
+            }
+            minA = A.Min();
+            sumofAllElementA = A.Sum();
+            maxA = A.Max();
 
-            /*1. double x;
-            double.TryParse(Console.ReadLine(), out x);
-            double y = 7 * Math.Pow(x,2) - 3 * x + 4;
-            Console.WriteLine(y);
-            Console.ReadKey();*/
+            Console.WriteLine("\nСумма всех элементов = {0}", sumofAllElementA);
+            Console.WriteLine("Минимальный элемент = {0}", minA);
+            Console.WriteLine("Максимальный элемент = {0}", maxA);
+            Console.WriteLine("Сумма четных элементов = {0}", sumEvenNumbers);
+            Console.ReadKey();
 
-            /* 2. int radius;
-             radius = int.Parse(Console.ReadLine());
-             double length;
-             length = 2 * Math.PI * radius;
-             Console.WriteLine(length);
+            Console.WriteLine();
+            Console.WriteLine("Двумернный массив: ");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Thread.Sleep(20);
+                    Random rnd = new Random();
+                    B[i, j] = rnd.Next(0, 10);
+                    Console.Write("{0} ", B[i, j]);
+                }
+                Console.WriteLine();
+            }
+            maxB = B[0, 0];
+            minB = B[0, 0];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    sumofAllElementB += B[i, j];
+                    if (j % 2 != 0)
+                    {
+                        sumOddColums += B[i, j];
+                    }
+                    if (minB > B[i, j])
+                    {
+                        minB = B[i, j];
+                    }
+                    if (maxB < B[i, j])
+                    {
+                        maxB = B[i, j];
+                    }
+                }
+
+            }
+            Console.WriteLine("\nСумма всех элементов = {0}", sumofAllElementB);
+            Console.WriteLine("Минимальный элемент = {0}", minB);
+            Console.WriteLine("Максимальный элемент = {0}", maxB);
+            Console.WriteLine("Сумма нечетных столбцов = {0}", sumOddColums);
+            Console.ReadKey(); */
+
+             /* 2. Console.WriteLine("Введите размер 1 массива: ");
+             int sizeArrayFirst = int.Parse(Console.ReadLine());
+             Console.WriteLine("Введите размер 2 массива: ");
+             int sizeArraySecond = int.Parse(Console.ReadLine());
+             int[] arrayFirst = new int[sizeArrayFirst];
+             int[] arraySecond = new int[sizeArraySecond];
+             for (int i = 0, j = 0; i < sizeArrayFirst && j < sizeArraySecond; i++, j++)
+             {
+                 Random random = new Random();
+                 Thread.Sleep(20);
+                 arrayFirst[i] = random.Next(0, 20);
+                 arraySecond[j] = random.Next(0, 20);
+             }
+             for (int i = 0; i < sizeArrayFirst; i++)
+             {
+                 Console.Write(arrayFirst[i] + " ");
+             }
+             Console.WriteLine();
+             for (int i = 0; i < sizeArraySecond; i++)
+             {
+                 Console.Write(arraySecond[i] + " ");
+             }
+            Console.WriteLine();
+            int[] sizeArrayThird = arrayFirst.Concat(arraySecond).Distinct().ToArray();
+             
+            foreach(var element in sizeArrayThird)
+            {
+                Console.Write(element + " ");
+            }
              Console.ReadKey();*/
-            /*3. double length;
-            double metr;
-            length = double.Parse(Console.ReadLine());
-            metr = length / 100;
-            Console.Write(metr);
-            Console.WriteLine(" метр");
-            Console.ReadKey();
-            */
-            /* 4. int day = 234;
-            double week = day / 7;
-            Console.Write("Прошло ");
-            Console.Write(week);
-            Console.WriteLine(" недель");
-            Console.ReadKey();*/
-            /*5. int number;
-            number = int.Parse(Console.ReadLine());
-            int digit1, digit2,sum;
-            digit1 = number / 10;
-            digit2 = number % 10;
-            sum = digit1 + digit2;
-            Console.WriteLine(digit1);
-            Console.WriteLine(digit2);
-            Console.Write("Сумма цифр = ");
-            Console.WriteLine(sum);
-            Console.ReadKey();*/
 
-            /*6. int number = int.Parse(Console.ReadLine());
-            int digit1, digit2, digit3, digit4;
-            digit1 = number / 1000;
-            digit2 = (number / 100) % 10;
-            digit3 = (number / 10) % 10;
-            digit4 = number % 10;
-            int sum , product;
-            sum = digit1 + digit2 + digit3 + digit4;
-            product = digit1 * digit2 * digit3 * digit4;
-            Console.WriteLine("Сумма = {0}", sum);
-            Console.WriteLine("Произведение = {0}", product);
+            /*3. var original = Console.ReadLine();
+             var reversed = new string(original.Reverse().ToArray());
+             if (original == reversed)
+             {
+                 Console.WriteLine("Строка является палиндромом");
+             }
+             else
+             {
+                 Console.WriteLine("Строка не является палиндромом");
+             }
+             Console.ReadKey();
+             */
+            /*4. Console.WriteLine("Напишите свое предложение");
+            string sentence = Console.ReadLine();
+            int wordCount = 1;
+            for(int i = 0; i < sentence.Length; i++)
+            {
+                if(sentence[i]==' ')
+                {
+                    wordCount++;
+                }
+            }
+            Console.WriteLine("Количество слов = " + wordCount);
             Console.ReadKey();*/
-
-            /* 7. int number = 456;
-            int digit1, digit2, digit3;
-            digit1 = number / 100;
-            digit2=(number/10)% 10;
-            digit3 = number % 10;
-            Console.Write("X = {0}{1}{2}", digit1, digit3, digit2);
+            /*5. int[,] array = new int[5, 5];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Random random = new Random();
+                    Thread.Sleep(20);
+                    array[i, j] = random.Next(-100, 100);
+                }
+            }
+            int maxArray = -100, minArray = 100, sumArray = 0; ;
+            int positionMaxI = 0, positionMaxJ = 0, positionMinI = 0, positionMinJ = 0;
+            bool count = false;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    sumArray += array[i, j];
+                    Console.Write(array[i, j] + "\t");
+                    if (array[i, j] > maxArray)
+                    {
+                        maxArray = array[i, j];
+                        positionMaxI = i;
+                        positionMaxJ = j;
+                    }
+                    if (minArray > array[i, j])
+                    {
+                        minArray = array[i, j];
+                        positionMinI = i;
+                        positionMinJ = j;
+                    }
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if ((i == positionMaxI && j == positionMaxJ) || (i == positionMinI && j == positionMinJ))
+                    {
+                        if (count)
+                        {
+                            count = false;
+                            continue;
+                        }
+                        else
+                        {
+                            count = true;
+                            continue;
+                        }
+                    }
+                    if (count)
+                    {
+                        sumArray += array[i, j];
+                    }
+                }
+            }
+            Console.WriteLine("Сумма элементов между минимальным и максимальным элементом = " + sumArray);
             Console.ReadKey();*/
-            //8
-            bool X = true, Y = false;
-            if (!X && !Y)
-            {
-                Console.WriteLine("A variant");
-            }
-            if(X ||(!X && Y))
-            {
-                Console.WriteLine("B variant");
-            }
-            if ((!X && Y) || Y)
-            {
-                Console.WriteLine("C variant");
-            }
-            Console.ReadKey();
         }
-
     }
 }
-
